a43753c0fa9fa5349e1a561e7337979f078b2043
s8abab <shabab@intoaec.ai>
2025-12-28 23:40:22 +0530

    refactor: Remove comment from BackButton component to streamline code

--- src/components/features/common/back-button.tsx
+++ src/components/features/common/back-button.tsx
@@ -2,7 +2,6 @@ import { ArrowLeftIcon } from "lucide-react";
 import Link from "next/link";
 
 export const BackButton = () => {
-    // this is a button that goes back to the previous page
     return (
         <Link href="/" className="text-black hover:opacity-70 transition-opacity flex items-center">
             <ArrowLeftIcon className="w-6 h-6" />
